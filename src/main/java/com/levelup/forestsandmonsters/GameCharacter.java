d33eacdfe1a30263fb6134abdd3af417d44fdac6
Vera Lester <limalester@yahoo.com>
2023-01-26 19:42:28 +0000

    setCharPosition file iscommited.

---
+++ src/main/java/com/levelup/forestsandmonsters/GameCharacter.java
@@ -0,0 +1,16 @@
+package com.levelup.forestsandmonsters;
+
+import java.awt.Point;
+
+public class GameCharacter {
+
+    Point position = null;
+    public Point getPosition() {
+        return this.position;
+    }
+
+    public void setPosition(Point position){
+        this.position = position;
+    }
+
+}
